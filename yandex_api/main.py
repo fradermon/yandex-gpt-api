26fdef9a26a112826a61b2f011450ace5e18b191
fradermon <fradermon@gmail.com>
2024-05-09 02:20:55 +0300

    append, dumps

--- yandex_api/main.py
+++ yandex_api/main.py
@@ -10,38 +10,26 @@ from error_handler import ClientError, ServerError, InvalidResultError
 with open(FILE_STAT_JSON, 'r', encoding='utf-8') as file_stat_json:
     file_stat = json.load(file_stat_json)
 
-with open(FILE_RES_JSON, 'r', encoding='utf-8') as file_res_json:
-    file_res = json.load(file_res_json)
-
 with open(FILE_NAMES_JSON, 'r', encoding='utf-8') as file_names_json:
     file_names = json.load(file_names_json)
 
 counter = 0
 
-# начало цикла
+# Start of the loop
 for id, status in file_stat.items():
-    if status == 'in progress': # if file_stat[i] == NO:
-
+    if status == 'no':
         name = next(item['name'] for item in file_names if item['id'] == int(id))
         scope = next(item['scope'] for item in file_names if item['id'] == int(id))
         print(name)
-        # print(scope)
 
-    # ЗДЕСЬ ЗАПРОС
+        file_res = []
 
         try:
-            response = requests.post(url = URL, headers=HEADERS, json=generate_prompt(name, scope))
+            response = requests.post(url=URL, headers=HEADERS, json=generate_prompt(name, scope))
             result = eval(response.text)
-            # print(result)
 
             answer = remove_special_symbols((result["result"]['alternatives'][0]['message']['text'])).lower()
             print(id, answer)
-
-            file_res_item = None
-            for item in file_res:
-                if item['id'] == int(id):
-                    file_res_item = item
-                    break
 
             found = False
             for category, pattern in CATEGORY_CHECK.items():
@@ -49,7 +37,8 @@ for id, status in file_stat.items():
                     for pat in pattern:
                         if re.search(pat, answer):
                             answer = category
-                            file_res_item['result'] = answer
+                            result_dict = {"id": id, "result": answer}
+                            file_res.append(result_dict)
                             file_stat[id] = 'yes'
                             found = True
                             break
@@ -58,36 +47,37 @@ for id, status in file_stat.items():
                 else:
                     if re.search(pattern, answer):
                         answer = category
-                        file_res_item['result'] = answer
+                        result_dict = {"id": id, "result": answer}
+                        file_res.append(result_dict)
                         file_stat[id] = 'yes'
                         found = True
                         break
 
             if not found:
                 answer = "другое"
-                file_res_item['result'] = answer
+                result_dict = {"id": id, "result": answer}
+                file_res.append(result_dict)
                 file_stat[id] = 'in progress'
 
         except (ClientError, ServerError, InvalidResultError) as e:
             error_type = e.__class__.__name__
             print(f"Ошибка при обработке '{name}': {e}")
             answer = str(e)
-            file_res_item = next(item for item in file_res if item['id'] == int(id))
-            file_res_item['result'] = answer
+            result_dict = {"id": id, "result": answer}
+            file_res.append(result_dict)
             file_stat[id] = 'error'
 
         except Exception as e:
             print(f"Ошибка при обработке группы '{name}': {e}")
             answer = str(e)
-            file_res_item = next(item for item in file_res if item['id'] == int(id))
-            file_res_item['result'] = answer
+            result_dict = {"id": id, "result": answer}
+            file_res.append(result_dict)
             file_stat[id] = 'error'
 
-        # обновляем файл file_res
-        with open(FILE_RES_JSON, 'w', encoding='utf-8') as file_res_json:
-            json.dump(file_res, file_res_json, ensure_ascii=False, indent=4)
+        with open(FILE_RES_JSON, 'a') as file_res_json:
+            for line in file_res:
+                file_res_json.write(json.dumps(line, ensure_ascii=False) + '\n')
 
-        # обновляем файл fle_stat
         with open(FILE_STAT_JSON, 'w', encoding='utf-8') as file_stat_json:
             json.dump(file_stat, file_stat_json, ensure_ascii=False, indent=4)
 
